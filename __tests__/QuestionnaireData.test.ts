1235013edf236e9f6f94930c38cf488e6929fec1
Gabriel Hess <heg2@bfh.ch>
2021-11-24 09:29:27 +0100

    test

--- __tests__/QuestionnaireData.test.ts
+++
@@ -1,8 +0,0 @@
-import { QuestionnaireData } from '../src/QuestionnaireData';
-import questionnaire from './questionnaire';
-
-test('Just testing', () => {
-    const qd = new QuestionnaireData(questionnaire)
-
-    expect(1 + 3).toBe(4);
-});
