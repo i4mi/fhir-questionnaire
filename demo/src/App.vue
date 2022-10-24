9f8cd3c94e61ab0d78451cfc2fbd09e691c1f923
Gabriel Hess <heg2@bfh.ch>
2022-10-24 17:30:08 +0200

    result modal

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -9,8 +9,7 @@
           <option
             v-for="questionnaire in questionnaires"
             :key="questionnaire.name"
-            @click="setQuestionnaire(questionnaire)"
-          >
+            @click="setQuestionnaire(questionnaire)">
             {{ questionnaire.name }}
           </option>
         </select>
@@ -25,21 +24,22 @@
           :question="question"
           :language="lang"
           :onAnswer="qData.updateQuestionAnswers"
-          :isSelected="qData.isAnswerOptionSelected"
-        />
+          :isSelected="qData.isAnswerOptionSelected" />
+        <button
+          :disabled="!qData"
+          @click="qData?.resetResponse()">
+          zurücksetzen
+        </button>
         <button @click="setAnswers">Antworten speichern</button>
       </div>
-      <p v-if="qData === undefined">
-        Es wurde noch kein Fragebogen ausgewählt.
-      </p>
+      <p v-if="qData === undefined">Es wurde noch kein Fragebogen ausgewählt.</p>
     </main>
 
     <!-- OWN QUESTIONNAIRE MODAL-->
     <div
       v-if="showOwnQuestionnaireModal"
       class="modal"
-      id="ownQuestionnaireModal"
-    >
+      id="ownQuestionnaireModal">
       <textarea v-model="ownQuestionnaire"></textarea>
       <button
         @click="
@@ -47,93 +47,90 @@
             showOwnQuestionnaireModal = false;
             ownQuestionnaire = '';
           }
-        "
-      >
+        ">
         abbrechen
       </button>
-      <button :disabled="ownQuestionnaire === ''" @click="loadOwnQuestionnaire">
+      <button
+        :disabled="ownQuestionnaire === ''"
+        @click="loadOwnQuestionnaire">
         laden
       </button>
     </div>
 
     <!-- RESPONSE MODAL-->
-    <div v-if="response" class="modal" id="responseModal">
-      <pre>{{ JSON.stringify(response) }}></pre>
+    <div
+      v-if="response"
+      class="modal"
+      id="response-modal">
+      <p>Hier ist die QuestionnaireResponse-Resource:</p>
+      <textarea v-model="response"></textarea>
       <button @click="response = undefined">schliessen</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import QuestionComponent from "./components/Question.vue";
-import ZARIT from "@/assets/questionnaires/zarit.json";
-import BLUEBOOK from "@/assets/questionnaires/bluebook.json";
-import SITUATION from "@/assets/questionnaires/situation.json";
-import { Questionnaire } from "@i4mi/fhir_r4";
-import { QuestionnaireData } from "@i4mi/fhir_questionnaire";
+import {defineComponent} from 'vue';
+import QuestionComponent from './components/Question.vue';
+import ZARIT from '@/assets/questionnaires/zarit.json';
+import BLUEBOOK from '@/assets/questionnaires/bluebook.json';
+import SITUATION from '@/assets/questionnaires/situation.json';
+import {Questionnaire, QuestionnaireResponse} from '@i4mi/fhir_r4';
+import {QuestionnaireData} from '@i4mi/fhir_questionnaire';
 
-const OWN_QUESTIONNAIRE = "[ eigener Fragebogen ]";
+const OWN_QUESTIONNAIRE = '[ eigener Fragebogen ]';
 
 export default defineComponent({
-  name: "App",
-  components: { QuestionComponent },
+  name: 'App',
+  components: {QuestionComponent},
   data() {
     return {
-      lang: "de",
-      availableLanguages: ["de", "en", "fr"],
+      lang: 'de',
+      availableLanguages: ['de', 'en', 'fr'],
       qData: undefined as QuestionnaireData | undefined,
       questionnaires: [
         {
-          name: "",
-          description: "",
-          questionnaire: undefined,
+          name: '',
+          description: '',
+          questionnaire: undefined
         },
         {
-          name: "ZARIT",
+          name: 'ZARIT',
           description:
-            "Der ZARIT Fragebogen dient pflegenden Angehörigen dazu, ihre eigene Belastung zu evaluieren. Der Fragebogen ist verfügbar in deutsch und französisch.",
-          questionnaire: ZARIT as Questionnaire,
+            'Der ZARIT Fragebogen dient pflegenden Angehörigen dazu, ihre eigene Belastung zu evaluieren. Der Fragebogen ist verfügbar in deutsch und französisch.',
+          questionnaire: ZARIT as Questionnaire
         },
         {
-          name: "Bluebook",
-          description:
-            "Irgend ein Neonatologie-Fragebogen, den ich im Netz gefunden habe.",
-          questionnaire: BLUEBOOK as Questionnaire,
+          name: 'Bluebook',
+          description: 'Irgend ein Neonatologie-Fragebogen, den ich im Netz gefunden habe.',
+          questionnaire: BLUEBOOK as Questionnaire
         },
         {
-          name: "Situation",
+          name: 'Situation',
           description:
-            "Fragebogen aus dem Corona Science Projekt, mit dem die aktuelle Situation der Befragen erfasst wird.",
-          questionnaire: SITUATION as Questionnaire,
+            'Fragebogen aus dem Corona Science Projekt, mit dem die aktuelle Situation der Befragen erfasst wird.',
+          questionnaire: SITUATION as Questionnaire
         },
         {
           name: OWN_QUESTIONNAIRE,
-          description: "Laden Sie Ihren eigenen FHIR Questionnaire.",
-          questionnaire: {} as Questionnaire,
-        },
+          description: 'Laden Sie Ihren eigenen FHIR Questionnaire.',
+          questionnaire: {} as Questionnaire
+        }
       ],
-      ownQuestionnaire: "",
+      ownQuestionnaire: '',
       showOwnQuestionnaireModal: false,
-      response: undefined,
+      response: undefined as string | undefined
     };
   },
   mounted() {},
   methods: {
-    setQuestionnaire(q: {
-      name: string;
-      description: string;
-      questionnaire: Questionnaire | undefined;
-    }): void {
+    setQuestionnaire(q: {name: string; description: string; questionnaire: Questionnaire | undefined}): void {
       if (q.questionnaire) {
-        if (confirm(q.description + "\n\nDiesen Fragebogen laden?")) {
+        if (confirm(q.description + '\n\nDiesen Fragebogen laden?')) {
           if (q.name === OWN_QUESTIONNAIRE) {
             this.showOwnQuestionnaireModal = true;
           } else {
-            this.qData = new QuestionnaireData(
-              q.questionnaire,
-              this.availableLanguages
-            );
+            this.qData = new QuestionnaireData(q.questionnaire, this.availableLanguages);
             console.log(this.qData.getQuestions());
           }
         }
@@ -143,25 +140,25 @@ export default defineComponent({
     },
     loadOwnQuestionnaire() {
       this.showOwnQuestionnaireModal = false;
-      this.qData = new QuestionnaireData(
-        JSON.parse(this.ownQuestionnaire),
-        this.availableLanguages
-      );
+      this.qData = new QuestionnaireData(JSON.parse(this.ownQuestionnaire), this.availableLanguages);
     },
     setAnswers(): void {
       if (!this.qData) return;
       try {
-        this.response = this.qData.getQuestionnaireResponse(this.lang, {
-            reset: true,
-            includeID: true,
-          });
+        this.response = JSON.stringify(
+          this.qData.getQuestionnaireResponse(this.lang, {
+            reset: false,
+            includeID: true
+          }),
+          null,
+          2
+        );
         console.log(this.response);
-        this.qData.resetResponse();
       } catch (error) {
-        console.log("Es ging etwas schief beim Questionnaire speichern", error);
+        console.log('Es ging etwas schief beim Questionnaire speichern', error);
       }
-    },
-  },
+    }
+  }
 });
 </script>
 
@@ -200,14 +197,27 @@ main {
 .modal {
   background-color: #ddd;
   width: 50%;
-  height: 10em;
-  position: absolute;
+  height: 20em;
+  position: fixed;
+  top: 5%;
+  color: black;
+  z-index: 100;
   margin-left: 25%;
   padding: 1em;
 }
 
-#ownQuestionnaireModal textarea {
+.modal p {
+  margin-top: 0;
+}
+
+button {
+  margin: 0.5em;
+  display: inline-block;
+  width: 25%;
+}
+
+.modal textarea {
   width: 100%;
-  height: 9em;
+  height: 18em;
 }
 </style>
