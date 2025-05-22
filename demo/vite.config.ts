2e57b017bc523ff7c2a480abd9caebcff68763c7
Gabriel Hess <heg2@bfh.ch>
2025-05-22 13:33:45 +0200

    demo app for gh pages

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fhir-questionnaire/demo/',
   plugins: [vue()],
   resolve: {
     alias: {
